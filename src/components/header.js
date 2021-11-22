f0b91be1c38c3fd123e650c7a07fc7d1948394bb
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-11-22 17:58:06 +0200

    Install Linters & Fix linters errors

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,21 @@
-import React from "react";
-import SvgIcon from "@mui/material/SvgIcon";
+import React from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
 import AppBar from '@mui/material/AppBar';
 
-function HomeIcon(props) {
+function HomeIcon() {
   return (
-    <SvgIcon {...props}>
+    <SvgIcon>
       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
     </SvgIcon>
   );
 }
 
-const Header = () => {
-  return (
-    <AppBar>
-      <div className="flex home">
+const Header = () => (
+  <AppBar>
+    <div className="flex home">
       <h1>Home</h1>
-        <HomeIcon />
-      </div>
-    </AppBar>
-  );
-};
+      <HomeIcon />
+    </div>
+  </AppBar>
+);
 export default Header;
