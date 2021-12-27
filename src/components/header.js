e12dfb77f8f6098e338f6523a8c7dede052a9973
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-27 18:45:13 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,6 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import SvgIcon from '@mui/material/SvgIcon';
 import AppBar from '@mui/material/AppBar';
-import { useNavigate } from 'react-router-dom';
 
 function HomeIcon() {
   return (
